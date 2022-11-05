9be1c7b592a3ee2a5313f0f86216991ea2d4f842
Cyan <204267667@qq.com>
2022-11-05 23:33:09 +0800

    home 大图预览

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,37 @@
 <template>
-<div>
-  <h2>
-    here are home page
-  </h2>
-</div>
+  <div class="home">
+    <!--    <img src="@/assets/img/winds01.jpg" alt=" 加载失败">-->
+    <div class="demo-image__preview">
+      <el-image
+          class="image"
+          :src="url"
+          :preview-src-list="srcList">
+      </el-image>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  data() {
+    return {
+      url: 'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg',
+      srcList: [
+        'https://fuss10.elemecdn.com/1/8e/aeffeb4de74e2fde4bd74fc7b4486jpeg.jpeg'
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
+.home {
+}
 
+.image {
+  display: block;
+  margin: 0 auto;
+  width: 50%;
+}
 </style>
